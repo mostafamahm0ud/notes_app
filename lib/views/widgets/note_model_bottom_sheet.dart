3c70c709c4430109c7d82a4d69a39d25e26e11fc
mostafamahm0ud <desha.2691998@gmail.com>
2023-04-26 02:07:28 +0200

    fix bottom model sheet

--- lib/views/widgets/note_model_bottom_sheet.dart
+++ lib/views/widgets/note_model_bottom_sheet.dart
@@ -24,7 +24,10 @@ class AddNoteBottomSheet extends StatelessWidget {
           return AbsorbPointer(
             absorbing: state is AddNoteLoading ? true : false,
             child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
+              padding: EdgeInsets.only(
+                  left: 16,
+                  right: 16,
+                  bottom: MediaQuery.of(context).viewInsets.bottom),
               child: const SingleChildScrollView(child: AddNotesForm()),
             ),
           );
