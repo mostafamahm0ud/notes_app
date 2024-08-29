aa86ee88bea70fdf86d8262d871f0f4d5b3867e5
Mostafa_Mahmoud <desha.2691998@gmail.com>
2024-08-29 17:04:52 +0300

    .

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/views/widgets/note_model_bottom_sheet.dart';
 import 'package:notes_app/views/widgets/notes_view_body.dart';
 
@@ -25,7 +23,7 @@ class NotesView extends StatelessWidget {
           Icons.add,
         ),
       ),
-      body:  NotesViewBody(),
+      body:  const NotesViewBody(),
     );
   }
 }
